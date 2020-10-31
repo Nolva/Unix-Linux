0ebd3d175c61ed0524fbe10816c9e3bf54765221
Alva <1090034020@qq.com>
2020-10-31 16:05:41 +0800

    修改READ.mE

--- 10/stdinredir2.c
+++ 10/stdinredir2.c
@@ -6,15 +6,19 @@
 
 // 显示了另外两种重定向标准输入的方法
 // 使用#define设置一个，#else设置另一个
-main() {
+main()
+{
     int fd;
     int newfd;
     char line[100];
 
     // read and print three lines
-    fgets(line, 100, stdin);printf("%s", line);
-    fgets(line, 100, stdin);printf("%s", line);
-    fgets(line, 100, stdin);printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
     // 输出stdin读取到的字符串
 
     // 重定向输入
@@ -23,18 +27,21 @@ main() {
     close(0);
     newfd = dup(fd);
 #else
-    newfd = dup2(fd,0); // 等价于#ifdef 关闭0，复制fd到0
+    newfd = dup2(fd, 0); // 等价于#ifdef 关闭0，复制fd到0
 #endif
-    if(newfd != 0) {
+    if (newfd != 0)
+    {
         fprintf(stderr, "Could not duplicate fd to 0\n");
         exit(1);
     }
-    close(fd);          // 关闭原始的文件描述符fd
+    close(fd); // 关闭原始的文件描述符fd
 
-    fgets(line, 100, stdin);printf("%s", line);
-    fgets(line, 100, stdin);printf("%s", line);
-    fgets(line, 100, stdin);printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
+    fgets(line, 100, stdin);
+    printf("%s", line);
 
     return 0;
-
 }
